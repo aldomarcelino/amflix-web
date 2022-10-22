f349dcaa30a5eb68cd99cae20df05343561b400c
aldomarcelino <aldo115marcelino@gmail.com>
2022-10-22 21:52:10 +0700

    fature updateing on main enitty

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,11 +1,23 @@
-const { Genre, Cast, MovieCast, Movie } = require("../models");
+const {
+  Genre,
+  Cast,
+  MovieCast,
+  Movie,
+  sequelize,
+  GenreMovie,
+  User,
+} = require("../models");
 const axios = require("axios");
 
 class MovieController {
   static async showAllMovie(req, res, next) {
     try {
       let data = await Movie.findAll({
-        include: { model: MovieCast, include: Cast },
+        include: [
+          { model: MovieCast, include: Cast },
+          { model: GenreMovie, include: Genre },
+          { model: User, attributes: ["email"] },
+        ],
       });
       res.status(200).json(data);
     } catch (err) {
@@ -31,25 +43,104 @@ class MovieController {
         name: el.name,
         profilePict: el.profile_path,
       }));
-      console.log(data, "<<<<<<<<<");
     } catch (err) {
       next(err);
     }
   }
 
   static async addNewMovies(req, res, next) {
+    const t = await sequelize.transaction();
     try {
-      const { title, imgUrl, synopsis, popularity, rating } = req.body;
-      await Movie.create({
+      const {
         title,
-        imgUrl,
         synopsis,
-        popularity,
+        trailerUrl,
         rating,
-        authorId: req.user.id,
-      });
+        name,
+        profilePict,
+        cast,
+        genre,
+      } = req.body;
+      console.log(req.body);
+      const movie = await Movie.create(
+        {
+          title,
+          trailerUrl,
+          synopsis,
+          popularity: 0,
+          rating,
+          authorId: req.user.id,
+        },
+        { transaction: t }
+      );
+      const genres = genre.map((el) => ({ MovieId: movie.id, GenreId: el }));
+      const data = [{ name, profilePict }, ...cast];
+      let actors = await Cast.bulkCreate(data, { transaction: t });
+      const casts = await actors.map((el) => ({
+        MovieId: movie.id,
+        CastsId: el.id,
+      }));
+      await GenreMovie.bulkCreate(genres, { transaction: t });
+      await MovieCast.bulkCreate(casts, { transaction: t });
+      await t.commit();
       res.status(201).json({ message: "Movie added successfully" });
     } catch (err) {
+      await t.rollback();
+      next(err);
+    }
+  }
+  static async updateTheMovie(req, res, next) {
+    const t = await sequelize.transaction();
+    try {
+      console.log(req.res);
+      const {
+        title,
+        synopsis,
+        trailerUrl,
+        rating,
+        name,
+        profilePict,
+        cast,
+        genre,
+      } = req.body;
+      console.log(req.body);
+      const theMovie = await Movie.findByPk(req.params.id);
+      if (!theMovie) throw { name: "Not_Found" };
+      const movie = await Movie.update(
+        {
+          title,
+          trailerUrl,
+          synopsis,
+          popularity: 0,
+          rating,
+          authorId: req.user.id,
+        },
+        { transaction: t, where: { id: req.params.id }, individualHooks: true }
+      );
+      const genres = genre.map((el) => ({ MovieId: movie.id, GenreId: el }));
+      const data = [{ name, profilePict }, ...cast];
+      await GenreMovie.destroy({
+        where: { MovieId: theMovie.id },
+        transaction: t,
+      });
+      await MovieCast.destroy({
+        where: { MovieId: theMovie.id },
+        transaction: t,
+      });
+      let actors = await Cast.bulkCreate(data, {
+        transaction: t,
+        updateOnDuplicate: ["id"],
+      });
+      const casts = await actors.map((el) => ({
+        MovieId: movie.id,
+        CastsId: el.id,
+      }));
+      await GenreMovie.bulkCreate(genres, { transaction: t });
+      await MovieCast.bulkCreate(casts, { transaction: t });
+      await t.commit();
+      res.status(201).json({ message: "Movie update successfully" });
+    } catch (err) {
+      await t.rollback();
       next(err);
     }
   }
--- routers/movie.js
+++ routers/movie.js
@@ -7,6 +7,7 @@ router.post("/", MovieController.addNewMovies);
 router.get("/genre", MovieController.showAllGenre);
 router.get("/casts", MovieController.showAllCast);
 router.post("/genre", MovieController.addNewGenre);
+router.put("/:id", MovieController.updateTheMovie);
 router.delete("/:id", MovieController.deleteTheMovie);
 router.put("/genre/:id", MovieController.updateTheGenre);
 router.delete("/genre/:id", MovieController.deleteTheGenre);
