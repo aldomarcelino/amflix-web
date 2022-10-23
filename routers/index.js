dd6e5d459777c242bf19803aa1904f1816944818
aldomarcelino <aldo115marcelino@gmail.com>
2022-10-23 17:08:18 +0700

    fixing bug fech on client user

--- routers/index.js
+++ routers/index.js
@@ -4,9 +4,11 @@ const authentication = require("../middleware/auth");
 const router = express.Router();
 const routerMovies = require("./movie");
 const errorHandler = require("../middleware/errhandler");
+const MovieController = require("../controllers/movie.controller");
 
 router.post("/signin", Controller.signIn);
 router.post("/signup", Controller.signUp);
+router.get("/public", MovieController.showAllMovieFree);
 router.use(authentication);
 router.use("/movies", routerMovies);
 router.use(errorHandler);
